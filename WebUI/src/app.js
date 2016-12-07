c1b2481a3a5cc6433833d0b5318008811c94cccb
martymazurik <marty@cloudcomputingassociates.com>
2016-12-07 10:53:40 -0800

    Update Website build TS modules
    Typescript wasn't compiling, ran from VS (failing) did npm install to
    build from cmd line

--- WebUI/src/app.js
+++ WebUI/src/app.js
@@ -5,23 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 //our root app component
-var core_1 = require("@angular/core");
-var platform_browser_1 = require("@angular/platform-browser");
-var http_1 = require("@angular/http");
-var ng_bootstrap_1 = require("@ng-bootstrap/ng-bootstrap");
+var core_1 = require('@angular/core');
+var platform_browser_1 = require('@angular/platform-browser');
+var http_1 = require('@angular/http');
+var ng_bootstrap_1 = require('@ng-bootstrap/ng-bootstrap');
 // Annotation section
 var App = (function () {
     function App() {
     }
+    App = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n    <div style=\"width: 50%; margin: 0 auto;\">     \n      <user-selector></user-selector>\n    </div>\n  ",
+        }), 
+        __metadata('design:paramtypes', [])
+    ], App);
     return App;
 }());
-App = __decorate([
-    core_1.Component({
-        selector: 'my-app',
-        template: "\n    <div style=\"width: 50%; margin: 0 auto;\">     \n      <user-selector></user-selector>\n    </div>\n  ",
-    })
-], App);
 exports.App = App;
 // Annotation section
 var UserInformation = (function () {
@@ -36,30 +40,32 @@ var UserInformation = (function () {
             _this.resourcegroups = res.json();
         });
     };
+    UserInformation = __decorate([
+        core_1.Component({
+            selector: 'user-selector',
+            templateUrl: 'resource-groups.html'
+        }), 
+        __metadata('design:paramtypes', [http_1.Http])
+    ], UserInformation);
     return UserInformation;
 }());
-UserInformation = __decorate([
-    core_1.Component({
-        selector: 'user-selector',
-        templateUrl: 'user-information-apps.html'
-    })
-], UserInformation);
 exports.UserInformation = UserInformation;
 var AppModule = (function () {
     function AppModule() {
     }
+    AppModule = __decorate([
+        core_1.NgModule({
+            imports: [
+                platform_browser_1.BrowserModule,
+                http_1.HttpModule,
+                ng_bootstrap_1.NgbModule.forRoot()
+            ],
+            declarations: [App, UserInformation],
+            bootstrap: [App]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
     return AppModule;
 }());
-AppModule = __decorate([
-    core_1.NgModule({
-        imports: [
-            platform_browser_1.BrowserModule,
-            http_1.HttpModule,
-            ng_bootstrap_1.NgbModule.forRoot()
-        ],
-        declarations: [App, UserInformation],
-        bootstrap: [App]
-    })
-], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.js.map
