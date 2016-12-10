f4f0cf8e3818be6b5b1737cb3848ec5d5b57bf08
martymazurik <marty@cloudcomputingassociates.com>
2016-12-09 18:57:19 -0800

    commit compiled ts changes so js files created

--- WebUI/src/app.js
+++ WebUI/src/app.js
@@ -5,29 +5,33 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 //our root app component
-var core_1 = require("@angular/core");
-var platform_browser_1 = require("@angular/platform-browser");
-var ng_bootstrap_1 = require("@ng-bootstrap/ng-bootstrap"); // ng bootstrap module
-var resource_group_1 = require("./resource-group");
+const core_1 = require('@angular/core');
+const platform_browser_1 = require('@angular/platform-browser');
+const ng_bootstrap_1 = require('@ng-bootstrap/ng-bootstrap'); // ng bootstrap module
+const resource_group_1 = require('./resource-group');
 // Annotation section
-var App = (function () {
-    function App() {
+let App = class App {
+    constructor() {
     }
-    return App;
-}());
+};
 App = __decorate([
     core_1.Component({
         selector: 'app',
-        template: "\n    <div style=\"width: 50%; margin: 0 auto;\">     \n      <resource-group></resource-group>\n    </div>\n  ",
-    })
+        template: `
+    <div style="width: 50%; margin: 0 auto;">     
+      <resource-group></resource-group>
+    </div>
+  `,
+    }), 
+    __metadata('design:paramtypes', [])
 ], App);
 exports.App = App;
-var AppModule = (function () {
-    function AppModule() {
-    }
-    return AppModule;
-}());
+let AppModule = class AppModule {
+};
 AppModule = __decorate([
     core_1.NgModule({
         imports: [
@@ -39,7 +43,8 @@ AppModule = __decorate([
             resource_group_1.ResourceGroupComponent
         ],
         bootstrap: [App]
-    })
+    }), 
+    __metadata('design:paramtypes', [])
 ], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.js.map
--- WebUI/src/resource-group.js
+++ WebUI/src/resource-group.js
@@ -5,27 +5,30 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 //res-group-component
-var core_1 = require("@angular/core");
-var ResourceGroupComponent = (function () {
-    function ResourceGroupComponent(_http) {
+const core_1 = require('@angular/core');
+const http_1 = require('@angular/http');
+let ResourceGroupComponent = class ResourceGroupComponent {
+    constructor(_http) {
         this._http = _http;
         this.url = 'http://dogservice.azurewebsites.net/api/azure/resourcegroups';
     } //end constructor 
-    ResourceGroupComponent.prototype.onClick = function () {
-        var _this = this;
+    onClick() {
         this._http.get(this.url) // getting the json back   TODO: should this be in a provider
-            .subscribe(function (res) {
-            _this.resourcegroups = res.json();
+            .subscribe((res) => {
+            this.resourcegroups = res.json();
         });
-    };
-    return ResourceGroupComponent;
-}());
+    }
+};
 ResourceGroupComponent = __decorate([
     core_1.Component({
         selector: 'resource-group',
-        templateUrl: 'resource-group.html'
-    })
+        templateUrl: './resource-group.html'
+    }), 
+    __metadata('design:paramtypes', [http_1.Http])
 ], ResourceGroupComponent);
 exports.ResourceGroupComponent = ResourceGroupComponent;
 //# sourceMappingURL=resource-group.js.map
