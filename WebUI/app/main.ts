cd42982cbfcf5c89b1755a54beff38d586b82b22
Marty Mazurik <marty@martymazurik.com>
2016-12-15 19:50:54 -0800

    remove extraneous App directory (we use src)

--- WebUI/app/main.ts
+++
@@ -1,8 +0,0 @@
-//main entry point
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .then(success => console.log('Bootstrap success'))
-  .catch(err => console.error(err));
